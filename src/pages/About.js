51621fa3a13483ba2bf681442204c96556437ed6
Ampy <rafiringa.a@gmail.com>
2023-07-21 12:09:45 +0300

    Cours React 1er partie

---
+++ src/pages/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A porpos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed ipsa
+        voluptatibus harum culpa blanditiis atque corporis voluptates numquam
+        vitae corrupti fugiat omnis provident, debitis possimus necessitatibus
+        praesentium aut, mollitia quas sunt explicabo. Corporis veniam nam eaque
+        odit facilis fugit, ex ab minus unde officia earum animi consequatur,
+        voluptate, placeat dicta qui dignissimos obcaecati! Numquam quae
+        inventore quia quos, vero beatae! Minima autem similique inventore iure
+        debitis, hic veritatis, deleniti, fugit totam reiciendis nobis aperiam
+        repudiandae labore repellendus? Quae eos odio commodi quis velit quas
+        esse illum, at vitae aliquam. Voluptate eum nisi placeat adipisci animi
+        ut at fugiat doloribus repellat?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus,
+        molestiae architecto ducimus accusantium magni optio ipsa, consequatur
+        vero atque tempore quidem nemo nulla ullam sequi facere quisquam quae
+        nesciunt odio numquam quo eos exercitationem pariatur! Ad error, tempora
+        sint sunt facilis nemo, omnis debitis inventore temporibus velit aperiam
+        eum officiis cupiditate porro iure fuga veritatis dolores, totam
+        eligendi iusto non! Esse voluptatum rerum error voluptates eaque amet
+        libero praesentium eveniet, fuga illo laudantium doloremque, natus quos
+        consectetur molestias ex odio cum recusandae saepe ut. Quos, blanditiis
+        sapiente? Dolorum, et facilis. Accusantium neque totam facere minima
+        perferendis eius fugit ullam ipsa temporibus. Quis dolorum sapiente
+        blanditiis mollitia, quaerat facilis vitae molestiae a debitis dicta?
+        Fugiat, dicta nisi! Libero dolore, dolorum architecto possimus animi
+        placeat itaque id ratione voluptatem, nesciunt reiciendis quo suscipit
+        quis sequi perspiciatis eius accusamus, necessitatibus dolores veniam
+        iusto debitis! Enim natus molestias doloribus, et at ipsam quidem
+        molestiae amet placeat suscipit harum ab corrupti aperiam, voluptates
+        magnam, voluptatibus adipisci ullam. Officiis ipsa rerum architecto
+        facilis recusandae quod in eligendi modi velit veritatis dolor earum
+        temporibus reprehenderit, assumenda incidunt sapiente? Dicta saepe
+        nihil, necessitatibus eaque blanditiis, magnam quas accusantium a culpa
+        iure modi at reprehenderit temporibus repellendus, atque nemo.
+      </p>
+    </div>
+  );
+};
+
+export default About;
